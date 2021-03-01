5656ec4de3d5d258bcccb94bed94751bae6beb03
NaumanYousafBajwa <50803418+NaumanYousafBajwa@users.noreply.github.com>
2021-03-01 13:00:51 +0500

    Add files via upload
    Yeh sara updated code hy. Integrate kar li ab khudi

---
+++ signin/signin.component.ts
@@ -0,0 +1,81 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SocialAuthService, GoogleLoginProvider } from "angularx-social-login";
+import { SignupServiceService } from 'src/app/services/signup-service.service';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+
+  model: any
+  flags = {
+    fieldTextType: false
+  }
+
+  signinService: SignupServiceService
+  constructor(http: HttpClient, private router: Router, private authService: SocialAuthService) {
+    this.model = {
+      email: "",
+      password: ""
+    }
+    this.signinService = new SignupServiceService(http)
+  }
+
+  ngOnInit(): void {
+    let signinUrl = "http://localhost:3000/signin"
+    let signupUrl = "http://localhost:3000/signup"
+    let signinWithGoogleUrl = "http://localhost:3000/signinWithGoogle"
+
+    this.signinService.postData(signinUrl, this.model).subscribe((data: any) => {
+      console.log(data)
+      if (data.text == "Already Logged in") {
+        this.router.navigateByUrl('dashboard')
+      }
+    })
+    this.authService.authState.subscribe((user) => {
+      console.log(user)
+      let dataToSend = {
+        email: user.email,
+        fullName: user.name,
+        provider: 'Google'
+      }
+
+      this.signinService.postData(signupUrl, dataToSend).subscribe((response) => {
+        console.log("Sign up Call in Google: ",response)
+        if (response.text === "Username already exists") {
+          this.signinService.postData(signinWithGoogleUrl, dataToSend).subscribe((response) => {
+            console.log("Sign in with Google Response: ",response)
+            if (response.text === "Logged in") {
+              this.router.navigateByUrl('dashboard')
+            }
+          })
+        } else if (response.text === "OK") {
+          this.router.navigateByUrl('dashboard')
+        }
+      })
+    })
+  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  onSubmit() {
+    console.log(this.model)
+    let signinUrl = "http://localhost:3000/signin"
+    this.signinService.postData(signinUrl, this.model).subscribe((data: any) => {
+      console.log(data)
+      if (data.text === "Logged in") {
+        this.router.navigateByUrl('dashboard')
+      }
+    })
+  }
+
+  toggleFieldTextType() {
+    this.flags.fieldTextType = !this.flags.fieldTextType
+  }
+}
